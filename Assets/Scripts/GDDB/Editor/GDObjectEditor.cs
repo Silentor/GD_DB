f3cc0d600548f447f59211a161832530545e0690
Silentor666 <silentor@users.noreply.github.com>
2024-03-25 17:02:04 +0200

    Implemented Components delimiter and foldout

--- Assets/Scripts/GDDB/Editor/GDObjectEditor.cs
+++ Assets/Scripts/GDDB/Editor/GDObjectEditor.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using Object = System.Object;
 using Random = UnityEngine.Random;
 
 namespace GDDB.Editor
@@ -10,11 +11,19 @@ namespace GDDB.Editor
     public class GDObjectEditor : UnityEditor.Editor
     {
         private GDObject _target;
-        private Int32 _lastSelectedComponentIndex = -1;
+        private Int32    _lastSelectedComponentIndex = -1;
+        private Material _componentDelimiterMat;
 
         protected virtual void OnEnable( )
         {
             _target = (GDObject)target;
+            var shader = Shader.Find("Hidden/Internal-Colored");
+            _componentDelimiterMat    = new Material(shader);
+        }
+
+        protected virtual void OnDisable( )
+        {
+            DestroyImmediate( _componentDelimiterMat );
         }
 
         public override void OnInspectorGUI( )
@@ -59,53 +68,80 @@ namespace GDDB.Editor
                 AssetDatabase.SaveAssetIfDirty( _target );
                 return;
             }
-            
+
+            DrawDelimiter( 10, 5 );
+
             //Draw GDComponents list
             for ( int i = 0; i < compsProp.arraySize; i++ )
             {
-                DrawComponentHeader( compsProp, i );
+                var foldoutStatus = DrawComponentHeader( compsProp, i );
 
-                var compProp = compsProp.GetArrayElementAtIndex( i );
-                var compEndProp = compProp.GetEndProperty();
-                if ( compProp.NextVisible( true ) )
+                if ( foldoutStatus )
                 {
-                    do
+                    var compProp    = compsProp.GetArrayElementAtIndex( i );
+                    var compEndProp = compProp.GetEndProperty();
+                    if ( compProp.NextVisible( true ) && !SerializedProperty.EqualContents( compProp, compEndProp ) )
                     {
-                        EditorGUILayout.PropertyField( compProp );
-                    }   
-                    while ( compProp.NextVisible( false ) && !SerializedProperty.EqualContents( compProp, compEndProp ) );
+                        do
+                        {
+                            EditorGUILayout.PropertyField( compProp );
+                        }   
+                        while ( compProp.NextVisible( false ) && !SerializedProperty.EqualContents( compProp, compEndProp ) );
+                    }
+
+                    DrawDelimiter( 10, 5 );
                 }
             }
 
             so.ApplyModifiedProperties();
 
-
-            DrawDelimiter();
             DrawComponentAddBtn();
         }
 
-        private void DrawDelimiter( )
-        {
-            GUILayout.Space( 5 );
-            GUILayout.Box( "", GUILayout.ExpandWidth( true ), GUILayout.Height( 1 ) );
-        }
-
-        private void DrawComponentHeader( SerializedProperty components, Int32 componentIndex )
+        private Boolean DrawComponentHeader( SerializedProperty components, Int32 componentIndex )
         {
             var compProp = components.GetArrayElementAtIndex( componentIndex );
 
             GUILayout.BeginHorizontal(  );
-            var compType          = compProp.managedReferenceValue.GetType();
-            var componentTypeName = compProp.managedReferenceFullTypename;
-            componentTypeName = !String.IsNullOrEmpty(componentTypeName) ? componentTypeName.Split('.').Last() : "Null";
-            GUILayout.Label( componentTypeName, Styles.BoldLabel );
-            GUILayout.FlexibleSpace();
-            GUILayout.Label( $"({compType.FullName}, {compType.Assembly.GetName().Name})", Styles.ItalicLabel );
-            if( GUILayout.Button( "X" ) )
+
+            var foldStatus = true;
+
+            if ( compProp.managedReferenceValue != null )
             {
-                RemoveComponent( componentIndex );
+                var compType = compProp.managedReferenceValue.GetType();
+
+                foldStatus = IsComponentFoldout( compProp.managedReferenceValue.GetType() );
+                EditorGUI.BeginChangeCheck();
+                foldStatus = EditorGUILayout.Foldout( foldStatus, /*GUIContent.none*/compType.Name, true , Styles.Foldout );
+                if( EditorGUI.EndChangeCheck() )
+                    SetComponentFoldout( compProp.managedReferenceValue.GetType(), foldStatus );
+                
+                //var componentTypeName = compProp.managedReferenceFullTypename;
+                //componentTypeName = !String.IsNullOrEmpty(componentTypeName) ? componentTypeName.Split('.').Last() : "Null";
+                //GUILayout.Label( componentTypeName, Styles.BoldLabel );
+                GUILayout.FlexibleSpace();
+                GUILayout.Label( $"({compType.FullName}, {compType.Assembly.GetName().Name})", Styles.ItalicLabel );
+                if( GUILayout.Button( "X" ) )
+                {
+                    RemoveComponent( componentIndex );
+                }
+            }
+            else
+            {
+                if( String.IsNullOrEmpty( compProp.managedReferenceFullTypename) )
+                    EditorGUILayout.HelpBox( "Component type cannot be found", MessageType.Error, wide: true);
+                else
+                    EditorGUILayout.HelpBox( "Component is null", MessageType.Error, wide: true);
+
+                GUILayout.FlexibleSpace();
+                if( GUILayout.Button( "X" ) )
+                {
+                    RemoveComponent( componentIndex );
+                }
             }
             GUILayout.EndHorizontal();
+
+            return foldStatus;
         }
 
         private void DrawComponentAddBtn( )
@@ -132,6 +168,27 @@ namespace GDDB.Editor
             }
 
             GUILayout.EndHorizontal();
+        }         
+
+        private void DrawDelimiter( Single spaceBefore, Single spaceAfter )
+        {
+            Rect rect = GUILayoutUtility.GetRect(10, 1000, spaceBefore + spaceAfter, spaceBefore + spaceAfter);
+            if ( Event.current.type == EventType.Repaint )
+            {
+                GUI.BeginClip( rect );
+                GL.PushMatrix();
+                GL.Clear( true, false, Color.black );
+
+                _componentDelimiterMat.SetPass(0);
+                GL.Begin(GL.LINES);
+                GL.Color(GUI.skin.settings.cursorColor);
+                GL.Vertex3(0,          spaceBefore, 0);
+                GL.Vertex3(rect.width, spaceBefore, 0);
+                GL.End();
+
+                GL.PopMatrix();
+                GUI.EndClip();
+            }
         }
 
         private void AddComponent( Type componentType )
@@ -147,10 +204,23 @@ namespace GDDB.Editor
             EditorUtility.SetDirty( _target );
         }
 
+        private Boolean IsComponentFoldout( Type componentType )
+        {
+            return  EditorPrefs.GetBool( componentType.Name, true );
+        }
+
+        private void SetComponentFoldout( Type componentType, Boolean state )
+        {
+            EditorPrefs.SetBool( componentType.Name, state );
+        }
+
+
         private static class Styles
         {
-            public static readonly GUIStyle ItalicLabel = new ( EditorStyles.label ) { fontStyle = FontStyle.Italic };
-            public static readonly GUIStyle BoldLabel   = EditorStyles.boldLabel;
+            public static readonly GUIStyle ItalicLabel        = new ( EditorStyles.label ) { fontStyle = FontStyle.Italic };
+            public static readonly GUIStyle BoldLabel          = EditorStyles.boldLabel;
+            public static readonly GUIStyle ComponentDelimiter = new ( GUI.skin.box ) { padding = new RectOffset()/*normal = new GUIStyleState(){background = Texture2D.blackTexture} */};
+            public static readonly GUIStyle Foldout = new ( EditorStyles.foldout ) { fontStyle = FontStyle.Bold };
         }
     }
 }
