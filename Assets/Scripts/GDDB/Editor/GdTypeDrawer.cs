2805f5c371f46c82d8abeb89919a14b55080e5b0
Silentor666 <silentor@users.noreply.github.com>
2024-05-12 02:39:22 +0300

    Workaround of an UIToolkit bug https://forum.unity.com/threads/createpropertygui-called-multiple-times.1572943/

--- Assets/Scripts/GDDB/Editor/GdTypeDrawer.cs
+++ Assets/Scripts/GDDB/Editor/GdTypeDrawer.cs
@@ -10,17 +10,21 @@ namespace GDDB.Editor
     [CustomPropertyDrawer( typeof(GdType) )]
     public class GdTypeDrawer : PropertyDrawer
     {
-        private VisualElement       _typeWidgetsContainer;
+        //Because of UIToolkit we cannot cache visual elements bug https://forum.unity.com/threads/createpropertygui-called-multiple-times.1572943/
+        //private          VisualElement              _root;
+        //private          VisualElement              _gdType;
+        //private          VisualElement              _toolbar;
 
         private          SerializedObject         _serializedObject;
         private          SerializedProperty       _dataProp;
-        private          VisualElement            _buttonsContainer;
+        
         private readonly GDTypeHierarchy          _typeHierarchy;
         private readonly GDTypeHierarchy.Category _typesRoot;
         private readonly StyleSheet               _gdTypeStyles;
-        private          VisualElement            _root;
+        
         private readonly GDObjectsFinder          _gdoFinder;
         private          GDObject                 _owner;
+
 
         public GdTypeDrawer( )
         {
@@ -36,70 +40,75 @@ namespace GDDB.Editor
             _owner            = (GDObject)_serializedObject.targetObject;
             _dataProp         = property.FindPropertyRelative( nameof(GdType.Data) );
 
-            _root = new VisualElement();
-            _root.name = "Root";
-            _root.style.flexDirection = FlexDirection.Row;
-            _root.styleSheets.Add( _gdTypeStyles );
+            var root      = new VisualElement();
+            root.name = "Root";
+            root.style.flexDirection = FlexDirection.Row;
+            root.styleSheets.Add( _gdTypeStyles );
 
             var label = new Label( property.displayName );
             label.style.flexBasis = new StyleLength( new Length( 41, LengthUnit.Percent ) );
             label.style.minWidth = 130;
-            _root.Add( label );
+            root.Add( label );
 
-            _typeWidgetsContainer                     = new VisualElement();
-            _typeWidgetsContainer.style.flexDirection = FlexDirection.Row;
-            _typeWidgetsContainer.style.flexGrow      = 1;
+            var gdType                     = new VisualElement();
+            gdType.name                = "GDType";
+            gdType.style.flexDirection = FlexDirection.Row;
+            gdType.style.flexGrow      = 1;
             //_typeWidgetsContainer.AddToClassList( "unity-base-field__aligned" );        
-            _root.Add( _typeWidgetsContainer );
+            root.Add( gdType );
             //var label = new Label( property.displayName );
             //label.AddToClassList( "unity-property-field__label" );
             //label.AddToClassList( "unity-base-field__label" );
             //label.AddToClassList( "unity-object-field__label" );
             //container.Add( label );
 
-            _buttonsContainer = new VisualElement();
-            _buttonsContainer.AddToClassList( "toolbar" );
-            _root.Add( _buttonsContainer );
+            var toolbar = new VisualElement();
+            toolbar.name = "Toolbar";
+            toolbar.AddToClassList( "toolbar" );
+            root.Add( toolbar );
 
-            RecreateProperty();
+            RecreateProperty( root );
 
-            return _root;
+            return  root;
         }
 
-        private void RecreateProperty( )
+        private void RecreateProperty( VisualElement root )
         {
-            _typeWidgetsContainer.Clear();
-            _buttonsContainer.Clear();
-            _root.RemoveFromClassList( "duplicateType" );
+            root.RemoveFromClassList( "duplicateType" );
+            var gdType = GetGDTypeFromRoot( root );
+            var toolbar = GetToolbarFromRoot( root );
+
+            gdType.Clear();
+            toolbar.Clear();
 
             if( _dataProp.intValue == 0 )
             {
-                CreateNoneType( _typeWidgetsContainer );
+                CreateNoneType( root );
             }
             else
             {
-                CreateWidgets( 0, _typesRoot );
+                CreateWidgets( root, 0, _typesRoot );
 
                 if ( _gdoFinder.IsDuplicatedType( _owner ) )
                 {
-                    var fixTypeBtn = new Button( FixType );
+                    var fixTypeBtn = new Button( () => FixType(root) );
                     fixTypeBtn.text                  = "";
                     fixTypeBtn.style.backgroundImage = new StyleBackground( Resources.Load<Sprite>( "build_24dp" ) );
                     fixTypeBtn.tooltip               = "Fix type";
                     fixTypeBtn.AddToClassList( "toolbar-square-button" );
-                    _buttonsContainer.Add( fixTypeBtn );
+                    toolbar.Add( fixTypeBtn );
                 }
 
-                var clearTypeBtn = new Button( ClearType );
+                var clearTypeBtn = new Button( () => ClearType(root) );
                 clearTypeBtn.text                  = "";
                 clearTypeBtn.style.backgroundImage = new StyleBackground( Resources.Load<Sprite>( "delete_forever_24dp" ) );
                 clearTypeBtn.tooltip               = "Clear type";
                 clearTypeBtn.AddToClassList( "toolbar-square-button" );
-                _buttonsContainer.Add( clearTypeBtn );
+                toolbar.Add( clearTypeBtn );
 
                 var myType = new GdType( _dataProp.intValue );
                 if( _gdoFinder.GDTypedObjects.Any( o => o.Type == myType && o != _owner ) )
-                    _root.AddToClassList( "duplicateType" );
+                    root.AddToClassList( "duplicateType" );
             }
         }
 
@@ -107,37 +116,40 @@ namespace GDDB.Editor
         private void CreateNoneType( VisualElement root )
         {
             var noneLabel = new Label("None");
-            _typeWidgetsContainer.Add( noneLabel );
+            var gdType    = GetGDTypeFromRoot( root );
+            gdType.Add( noneLabel );
 
-            var addTypeBtn = new Button( AssignType );
+            var addTypeBtn = new Button( () => AssignType(root) );
             addTypeBtn.text = "";
             addTypeBtn.AddToClassList( "toolbar-square-button" );
             addTypeBtn.style.backgroundImage = new StyleBackground( Resources.Load<Sprite>( "add_24dp" ) );
             addTypeBtn.tooltip = "Assign type";
-            _buttonsContainer.Add( addTypeBtn );
+            var toolbar = GetToolbarFromRoot( root );
+            toolbar.Add( addTypeBtn );
         }
 
-        private void CreateWidgets( Int32 index, GDTypeHierarchy.Category category )
+        private void CreateWidgets( VisualElement root, Int32 index, GDTypeHierarchy.Category category )
         {
             if( index >= 4 )
                 return;
 
+            var gdType = GetGDTypeFromRoot( root );
             //Debug.Log( $"Creating widget at {index} position" );
 
             //Clear this widget and all next
-            while ( _typeWidgetsContainer.childCount > index )
-                _typeWidgetsContainer.RemoveAt( index );
+            while ( gdType.childCount > index )
+                gdType.RemoveAt( index );
 
-            var gdType = new GdType( _dataProp.intValue );
-            var widget = CreateCategoryField( category, gdType[ index ], index );
-            _typeWidgetsContainer.Add( widget );
+            var gdTypeValue = new GdType( _dataProp.intValue );
+            var widget = CreateCategoryField( root, category, gdTypeValue[ index ], index );
+            gdType.Add( widget );
 
             //Create next widget(s)
-            var nextCategory = category?.FindItem( gdType[ index ] )?.Subcategory;
-            CreateWidgets( index + 1, nextCategory );
+            var nextCategory = category?.FindItem( gdTypeValue[ index ] )?.Subcategory;
+            CreateWidgets( root, index + 1, nextCategory );
         }
 
-        private VisualElement CreateCategoryField( GDTypeHierarchy.Category category, Int32 value, Int32 index )
+        private VisualElement CreateCategoryField( VisualElement root, GDTypeHierarchy.Category category, Int32 value, Int32 index )
         {
             VisualElement result = null;
             if ( category == null )         //Default Int8 field
@@ -145,24 +157,24 @@ namespace GDDB.Editor
                 result                = new IntegerField( 3 ){ value = value };
                 result.style.minWidth = 50;
                 result.style.flexGrow = 1;
-                result.RegisterCallback<ChangeEvent<Int32>, Int32>( OnChangeEnumWidget, index  );
+                result.RegisterCallback<ChangeEvent<Int32>>( OnChangeEnumWidget );
 
-                //Debug.Log( $"Created int field, value {value}" );
+                Debug.Log( $"Created int field, index {index}, value {value}" );
             }
             else if ( category.Type == GDTypeHierarchy.CategoryType.Enum )
             {
                 result = new PopupField<Int32>( category.Items.Select( i => i.Value ).ToList(), value, i => category.FindItem( i ).Name,
                         i => category.FindItem( i ).Name );
                 result.style.flexGrow = 1;
-                result.RegisterCallback<ChangeEvent<Int32>, Int32>( OnChangeEnumWidget, index  );
+                result.RegisterCallback<ChangeEvent<Int32>>( OnChangeEnumWidget );
 
-                //Debug.Log( $"Created popup field for {category.UnderlyingType} category, value {category.FindItem( value ).Name}" );
+                Debug.Log( $"Created popup field, index {index} for {category.UnderlyingType} category, value {category.FindItem( value ).Name}" );
             }
 
             return result;
 
             //Rebuild widgets after change
-            void OnChangeEnumWidget( ChangeEvent<Int32> evt, Int32 index )
+            void OnChangeEnumWidget( ChangeEvent<Int32> evt )
             {
                 _serializedObject.Update();
                 var gdType = new GdType( _dataProp.intValue );
@@ -171,36 +183,47 @@ namespace GDDB.Editor
                 _serializedObject.ApplyModifiedProperties();
 
                 var nextCategory = category?.FindItem( evt.newValue )?.Subcategory;
-                CreateWidgets( index + 1, nextCategory );
+                CreateWidgets( root, index + 1, nextCategory );
             }
         }
 
-        private void AssignType( )
+        private void AssignType( VisualElement root )
         {
             _serializedObject.Update();
             _dataProp.intValue = 1;
             _serializedObject.ApplyModifiedProperties();
-            RecreateProperty();
+            RecreateProperty( root );
         }
 
-        private void ClearType( )
+        private void ClearType( VisualElement root )
         {
             _serializedObject.Update();
             _dataProp.intValue = 0;
             _serializedObject.ApplyModifiedProperties();
-            RecreateProperty();
+            RecreateProperty( root );
         }
 
-        private void FixType( )
+        private void FixType( VisualElement root )
         {
             _serializedObject.Update();
             if ( _gdoFinder.FindFreeType( new GdType( _dataProp.intValue ), out var newType ) ) 
             {
                 _dataProp.intValue = (Int32)newType.Data;
                 _serializedObject.ApplyModifiedProperties();
-                RecreateProperty();
+                RecreateProperty( root );
             }
         }
+
+        private VisualElement GetGDTypeFromRoot( VisualElement root )
+        {
+            return root.Q<VisualElement>( "GDType" );
+        }
+
+        private VisualElement GetToolbarFromRoot( VisualElement root )
+        {
+            return root.Q<VisualElement>( "Toolbar" );
+        }
+
 
     }
 
