cd9acc94d5319d43ff925178ed0bac3125896cbc
Silentor <SilentorWork@gmail.com>
2024-08-16 19:10:40 +0300

    WIP

--- Assets/Scripts/TestCategories.cs
+++ Assets/Scripts/TestCategories.cs
@@ -3,61 +3,61 @@ using GDDB;
 
 namespace TestGdDb
 {
-    [Category]
-    public enum MainCategory
-    {
-        Game,
-        Mobs,
-        Items,
-        Weapons,
-        Spells,
-        InApps,
-        Locations = 10
-    }
-
-    [Category( typeof(MainCategory), (Int32)MainCategory.Game)]
-    public enum EGame
-    {
-        GameModes,
-        Currencies
-    }
-
-    [Category(typeof(EGame), (Int32)EGame.Currencies)]
-    public enum ECurrency
-    {
-        Silver,
-        Gold,
-        Gems,
-        Bucks
-    }
-
-    [Category( MainCategory.Mobs )]
-    public enum EMobs
-    {
-        Humans,
-        Orcs,
-        Elves
-    }
-
-    public enum EItems
-    {
-        Consumables,
-        Reagents,
-        QuestItems,
-
-    }
-
-    public enum EWeapons
-    {
-        Swords,
-        Bows,
-        Axes
-    }
-
-    public enum EInApps
-    {
-        Skins,
-        Weapons,
-        Consumables
-    }
+    // [Category]
+    // public enum MainCategory
+    // {
+    //     Game,
+    //     Mobs,
+    //     Items,
+    //     Weapons,
+    //     Spells,
+    //     InApps,
+    //     Locations = 10
+    // }
+    //
+    // [Category( typeof(MainCategory), (Int32)MainCategory.Game)]
+    // public enum EGame
+    // {
+    //     GameModes,
+    //     Currencies
+    // }
+    //
+    // [Category(typeof(EGame), (Int32)EGame.Currencies)]
+    // public enum ECurrency
+    // {
+    //     Silver,
+    //     Gold,
+    //     Gems,
+    //     Bucks,
+    // }
+    //
+    // [Category( MainCategory.Mobs )]
+    // public enum EMobs
+    // {
+    //     Humans,
+    //     Orcs,
+    //     Elves
+    // }
+    //
+    // public enum EItems
+    // {
+    //     Consumables,
+    //     Reagents,
+    //     QuestItems,
+    //
+    // }
+    //
+    // public enum EWeapons
+    // {
+    //     Swords,
+    //     Bows,
+    //     Axes
+    // }
+    //
+    // public enum EInApps
+    // {
+    //     Skins,
+    //     Weapons,
+    //     Consumables
+    // }
 }
